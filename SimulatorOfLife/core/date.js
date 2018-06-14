57b49b46fd1f4014ec78c5506ae217d1487db0e7
Ilya <ovchildmy@gmail.com>
2018-06-14 21:41:05 +0300

    Date update

---
+++ SimulatorOfLife/core/date.js
@@ -0,0 +1,22 @@
+var day=1;
+var month=1;
+var year=1;
+
+
+$(".nextDay").on("click",function(){
+	var title = $(".nextDay").attr("title");
+
+	if(day==30){
+		day=1
+		month+=1;
+	}else{
+		day++;
+	}
+
+	if(month==13){
+		month=1;
+		year++;
+	}
+
+	$(".nextDay").attr("title", day+" day, "+month+" month, "+year+" year");
+})
--- SimulatorOfLife/js/job.js
+++ SimulatorOfLife/js/job.js
@@ -2,19 +2,19 @@
 
 $('.workGet').on("click", function(){
 	localStorage.jobStatus = "true";
-	console.log(localStorage.jobStatus)
+	// console.log(localStorage.jobStatus)
 })
 
 $('.workQuit').on("click", function(){
 	localStorage.jobStatus = "false";
-	console.log(localStorage.jobStatus)
+	// console.log(localStorage.jobStatus)
 })
 
 function jobFunc(){
 	var money = +$('.money__cash').text();
 	if(localStorage.jobStatus == "true"){
 		$('.money__cash').text(money+60);
-		console.log(123)
+		// console.log(123)
 	}
 
 }
