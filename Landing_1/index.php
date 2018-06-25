8f79ae256c402a854cc6cfa8ee3105d1ab2155b2
Ilya <ovchildmy@gmail.com>
2018-06-25 13:18:13 +0300

    Landing_1 was started

---
+++ Landing_1/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style/uikit-rtl.css">
+	<link rel="stylesheet" type="text/css" href="style/uikit.css">
+	<link rel="stylesheet" type="text/css" href="style/header.css">
+	<link rel="stylesheet" type="text/css" href="style/aboutUs.css">
+	<link rel="stylesheet" type="text/css" href="style/personal.css">
+	<link rel="stylesheet" type="text/css" href="style/prices.css">
+	<link rel="stylesheet" href="OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css">
+	<link rel="stylesheet" href="OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
+	<title></title>
+</head>
+<body>
+
+	<div class="headerWrap">
+		<div class="header">
+			<div class="headName">landing</div>
+			<div class="header_contacts">
+				<ul>
+					<li>link</li>
+					<li>link</li>
+					<li>link</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="aboutUs">
+		<div class="aboutUsHeader">About Us</div>
+
+		<img src="img/aboutUs_1.jpg">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+	</div>
+
+	<div class="personalWrap owl-carousel owl-theme">
+		<div class="personal">
+			<img src="img/personal.png">
+			<div>Lorem ipsum ipsum ipsum ipsum ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua.</div>
+		</div>
+
+		<div class="personal">
+			<img src="img/personal_2.jpg">
+			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore ipsum ipsum ipsum magna aliqua.</div>
+		</div>
+
+		<div class="personal">
+			<img src="img/personal_3.jpg">
+			<div>Lorem ipsum dolor sit amet, consectetur ipsum ipsum ipsum adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua.</div>
+		</div>
+
+		<div class="personal">
+			<img src="img/personal_4.jpg">
+			<div>Lorem ipsum dolor sit amet, consectetur ipsum ipsum ipsum adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua.</div>
+		</div>
+
+		<div class="personal">
+			<img src="img/personal_5.jpg">
+			<div>Lorem ipsum dolor sit amet, consectetur ipsum ipsum ipsum adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua.</div>
+		</div>
+
+		<div class="personal">
+			<img src="img/personal_6.jpg">
+			<div>Lorem ipsum dolor sit amet, consectetur ipsum ipsum ipsum adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua.</div>
+		</div>
+	</div>
+
+	<div class="pricesBackground">
+		<div class="prices">
+			<div class="pricesNav"><span>service 1</span><span>service 2</span><span>service 3</span></div>
+		</div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+	<script type="text/javascript" src="lib/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="lib/uikit.js"></script>
+	<script type="text/javascript" src="lib/uikit-icons	.js"></script>
+	<!-- <script src="jquery.min.js"></script> -->
+	<script src="OwlCarousel2-2.3.4/dist/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="lib/core.js"></script>
+</body>
+</html>
