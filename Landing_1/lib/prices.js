fac2037880cb8223f3be4d8fe7f53049d0828bc4
Ilya <ovchildmy@gmail.com>
2018-06-26 18:21:33 +0300

    Mail update, footer is ready, services design

--- Landing_1/lib/prices.js
+++ Landing_1/lib/prices.js
@@ -1,36 +1,12 @@
 $(".pricesNav div").on("click",function(){
+	var curPrices = $(".pricesNav div");
+	var index = $(this).index();
+	var prices = $(".serviceWrap");
+
 	$(".pricesNav div").removeClass("currentPrice");
 	$(this).addClass("currentPrice");
+	$(".serviceWrap").hide();
+	$(".serviceWrap").eq(index).show();
 })
 
 $(".pricesNav div").first().click();
-
-$(".pricesNav div").on("click",function(){
-	var curPrices = $(".pricesNav div");
-
-
-
-
-	$(".pricesNav div").each(function(index){
-		// console.log($(".pricesNav div").eq(index))
-
-			// console.log(this)
-		if($(".pricesNav div")[index]==$(this)){
-			console.log($(".pricesNav div")[index])
-		}
-	})
-
-	// console.log($(this).eq(1))
-	// console.log($(".currentPrice"))
-
-
-	// for(var i=0; i < $(".pricesNav div").length; i++){
-	// 	if($(".pricesNav div").eq(i) == $(".currentPrice")){
-	// 		console.log(i)
-	// 	}
-	// }
-
-
-	// $(".serviceWrap").hide()
-})
-
