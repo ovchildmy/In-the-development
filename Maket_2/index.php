c4a14ca5cd4a0f10186e31eb09aba55270d4670e
Ilya <ovchildmy@gmail.com>
2018-07-05 22:46:57 +0300

    Header is ready

---
+++ Maket_2/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="style/uikit-rtl.css">
+	<link rel="stylesheet" type="text/css" href="style/uikit.css">
+	<link rel="stylesheet" type="text/css" href="style/core.css">
+	<link rel="stylesheet" type="text/css" href="style/header.css">
+	<title></title>
+</head>
+<body>
+
+	<div class="headerWrap">
+		<div class="header">
+			<div class="container">
+				<div class="header-top">
+					Salem
+					<i class="fa fa-bars" aria-hidden="true"></i>
+				</div>
+
+				<div>
+					<div>we <span>love</span> to write code</div>
+					<div></div>
+					<div class="header_content">
+						Salem is a new and modern page Wordpress theme, designed and aimed at creative teams that want to showcase your products and services in a new and creative way
+					</div>
+					<div>
+						<i class="fa fa-arrow-circle-o-down" aria-hidden="true"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+	<script type="text/javascript" src="lib/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="lib/uikit.js"></script>
+	<script type="text/javascript" src="lib/uikit-icons	.js"></script>
+	<script type="text/javascript" src="lib/core.js"></script>
+</body>
+</html>
