540ffb46665fc7bfbef717bcf24950544bb19c10
Ilya <ovchildmy@gmail.com>
2018-07-18 23:34:04 +0300

    E-shop started, header is nearly ready

---
+++ e-shop/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="style/uikit-rtl.css">
+	<link rel="stylesheet" type="text/css" href="style/uikit.css">
+	<link rel="stylesheet" type="text/css" href="style/core.css">
+	<link rel="stylesheet" type="text/css" href="style/header.css">
+	<meta charset="utf-8">
+	<title>E-Shop</title>
+</head>
+<body>
+
+	<div class="header">
+		<div class="container">
+			<div class="header-name">E-shop</div>
+			<div class="header-ico">
+				<div class="header-basket"><i class="fa fa-shopping-cart" aria-hidden="true"></i></div>
+				
+				<ul>
+					<li class="header-user header-list"><i class="header-user__ico fa fa-user-circle-o" aria-hidden="true"></i>
+						<ul>
+							<li>Войти</li>
+							<li>Регистрация</li>
+						</ul>
+
+					</li>
+				</ul>
+
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+	<script type="text/javascript" src="lib/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="lib/uikit.js"></script>
+	<script type="text/javascript" src="lib/uikit-icons	.js"></script>
+	<script type="text/javascript" src="lib/core.js"></script>
+</body>
+</html>
